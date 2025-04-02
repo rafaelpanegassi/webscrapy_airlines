cf8578be3a854226152073d078e9e2ba88796122
Rafael Panegassi <rm.panegassi@gmail.com>
2025-04-02 14:47:41 -0300

    feat: new function extract

--- src/crawler/abstract_crawler.py
+++ src/crawler/abstract_crawler.py
@@ -2,54 +2,41 @@ from abc import ABC, abstractmethod
 
 from loguru import logger
 
-from src.tools.mongodb import MongoConnection
-from src.tools.redis import RedisClient
-from src.tools.browser_provider import BrowserProvider
+from tools.browser_provider import BrowserProvider
+from tools.mongodb import MongoConnection
+from tools.redis import RedisClient
 
 
 class AbstractCrawler(ABC):
-    """
-    Abstract base class for web crawlers.
-    """
+
     def __init__(self):
         self.redis = RedisClient.get()
         self.mongo = MongoConnection()
-        self.browser = BrowserProvider()
+        self.browser = BrowserProvider().get_browser()
+
+    def execute_steps(self):
+        steps = self.steps["script"]
+        if steps["before"]:
+            self.dynamic_steps(steps["before"])
+        if steps["main"]:
+            self.dynamic_steps(steps["main"])
+        if steps["after"]:
+            self.dynamic_steps(steps["after"])
 
     @abstractmethod
-    def execute_main(self):
-        """
-        Execute the main crawling process.
-        """
-        pass
-
-    @abstractmethod
-    def execute_before(self):
-        """
-        Execute any pre-crawling setup.
-        """
-        pass
-
-    @abstractmethod
-    def execute_after(self):
-        """
-        Execute any post-crawling cleanup.
-        """
+    def dynamic_steps(self, steps):
         pass
 
     def get_step(self, key):
-        """
-        Get the current step of the crawler.
-        """
         steps = None
         try:
             steps = self.redis.get(key)
         except Exception as e:
-            logger.error(f'Error collecting data from Redis: {e}')
-            return steps
+            logger.error(f'Impossible to catch data from Redis: {e}')
+        return steps
 
     def save_data(self, data):
         try:
             self.mongo.save_dataframe(data)
         except Exception as e:
-            logger.error(f'Error saving to MongoDB: {e}')
+            logger.error(f'Error to save in MongoDB: {e}')
